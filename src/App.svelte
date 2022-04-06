62c8438bc21441c20665d420a46d2c94f5d51091
allehS <arisyt2004@gmail.com>
2022-04-06 23:53:24 +0700

    action test

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,22 @@
 <script>
-	const json = fetch("../2021-1.json") 
-	
+	const getData = async (year, test) => {
+		const data = await fetch(`https://raw.githubusercontent.com/shellawa/scoreScraper/master/data/${year}/${test}.json`)
+		return data.json()
+	}
 </script>
+
+<div class="container">
+{#await getData("2021","1")}
+	<p>waitin</p>
+{:then data}
+	<table class="table table-striped">
+		{#each data as score}
+			<tr>
+				{#each score as info}
+					<td>{info}</td>
+				{/each}
+			</tr>
+		{/each}
+	</table>
+{/await}
+</div>
